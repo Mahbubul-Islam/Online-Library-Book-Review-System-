5c2f809df16d6d610882042db1dac784500b1c55
Mahbubul Islam <mahbubulislamshiam48@gmail.com>
2025-10-05 11:33:32 +0600

    Enhance admin and models for books: register models, add ordering, and implement average rating calculation

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
-
+from .models import Category, Book, Review
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'category')
+    search_fields = ('title', 'author')
+    list_filter = ('category',)
+    
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('id', 'book', 'user', 'rating', 'created_at')
+    search_fields = ('book__title', 'user__username')
+    list_filter = ('rating', 'created_at')
+
+
--- books/models.py
+++ books/models.py
@@ -1,3 +1,53 @@
 from django.db import models
-
+import os
+from django.contrib.auth.models import User
 # Create your models here.
+
+# save files according to instance name
+def book_cover_directory_name(instance, filename):
+    return os.path.join('Books/media', instance.title, filename)
+
+# 1. Category
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+    
+# 2. Books
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=150)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='books') # one to many. One category can have many books
+    cover_image = models.ImageField(upload_to=book_cover_directory_name, blank=True, null=True) 
+    description = models.TextField() 
+    created_at = models.DateTimeField(auto_now_add=True) 
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
+    def average_rating(self):
+        reviews = self.reviews.all()
+        if not reviews.exists():
+            return 0
+        return round(sum([review.rating for review in reviews]) / reviews.count(), 1)
+
+# 3. Review
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  
+    book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='reviews')
+    comment = models.TextField()
+    rating = models.PositiveIntegerField(default=0)  # rating from 1 to 5 (optional)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"Review by {self.user.username} on {self.book.title}"
