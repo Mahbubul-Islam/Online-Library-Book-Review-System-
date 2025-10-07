228ede989a88e103afe376f4e7da8968db6dbf0b
Mahbubul Islam <mahbubulislamshiam48@gmail.com>
2025-10-07 13:43:05 +0600

    Update static file settings to define STATIC_ROOT and conditionally set STATICFILES_DIRS

--- online_library/settings.py
+++ online_library/settings.py
@@ -126,7 +126,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Only add to STATICFILES_DIRS if the directory exists
+STATICFILES_DIRS = []
+if (BASE_DIR / 'static').exists():
+    STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Media files (user-uploaded content)
 MEDIA_URL = '/media/'
